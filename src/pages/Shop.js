93d53e2cd87d35cb9db759827b543b56c21d82ab
yyeexin <248480904@qq.com>
2019-12-10 18:57:44 +0800

    commit

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,9 +1,35 @@
+import { Table } from 'antd'
 import React from 'react'
-import { connect } from 'dva'
+import { useAntdTable } from '@umijs/hooks'
 
-const Shop = props => {
-	console.log(props)
-	return <div>Shop Page</div>
+const getTableData = ({ current, pageSize }) =>
+	fetch(`https://randomuser.me/api?results=55&page=${current}&size=${pageSize}`)
+		.then(res => res.json())
+		.then(res => ({
+			total: res.info.results,
+			data: res.results
+		}))
+
+export default () => {
+	const { tableProps } = useAntdTable(getTableData, [], {})
+	console.log(tableProps)
+	const columns = [
+		{
+			title: 'name',
+			dataIndex: 'name.last'
+		},
+		{
+			title: 'email',
+			dataIndex: 'email'
+		},
+		{
+			title: 'phone',
+			dataIndex: 'phone'
+		},
+		{
+			title: 'gender',
+			dataIndex: 'gender'
+		}
+	]
+	return <Table columns={columns} rowKey='email' {...tableProps} />
 }
-
-export default connect(({ shop }) => ({ shop }))(Shop)
