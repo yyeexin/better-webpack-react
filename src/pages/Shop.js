d9fd46d5ddd435e4a716340de976eeb1fdb01608
yyeexin <248480904@qq.com>
2020-01-06 19:33:32 +0800

    commit

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -4,25 +4,23 @@ import styled from 'styled-components'
 import { hot } from 'react-hot-loader/root'
 import { Switch, Button } from 'antd'
 
+const StyledButton = styled(Button)`
+	border-radius: ${props => (props.radius % 2 === 0 ? '0' : '20px')};
+	width: 100px;
+	height: 50px;
+`
+
 const Shop = props => {
+	const [count, setCount] = useState(0)
+
 	function onChange(val) {
 		console.log(val)
 	}
 
-	const [count, setCount] = useState(0)
-
-	const StyledButton = styled(Button)`
-		background: ${props => (props.color ? 'palevioletred' : 'white')};
-		width: 100px;
-		height: 50px;
-		&.ant-btn:focus {
-			// background-color: yellow;
-		}
-	`
 	return (
 		<div>
 			<Switch defaultChecked onChange={onChange} />
-			<StyledButton type='primary' danger color={count % 2 === 0} onClick={() => setCount(count + 1)}>
+			<StyledButton type='primary' radius={count} onClick={() => setCount(count + 1)}>
 				{count}
 			</StyledButton>
 		</div>
