f41fd7db7e59edd4827cd99fc38a999a5330713c
yyeexin <248480904@qq.com>
2020-01-10 13:14:29 +0800

    commit

--- src/pages/User.js
+++ src/pages/User.js
@@ -1,6 +1,5 @@
-// import React from 'react'
+import React, { useState, useEffect, useReducer } from 'react'
 import styled from 'styled-components'
-
 const User = () => {
 	// 创建一个 Title 组件,它将渲染一个附加了样式的 <h1> 标签
 	const Title = styled.h1`
@@ -24,4 +23,84 @@ const User = () => {
 	)
 }
 
-export default User
+const Example = () => {
+	const [count, setCount] = useState(0)
+
+	function handleAlertClick() {
+		setTimeout(() => {
+			alert('You clicked on: ' + count)
+		}, 3000)
+	}
+
+	return (
+		<div>
+			<p>You clicked {count} times</p>
+			<button onClick={() => setCount(count + 1)}>Click me</button>
+			<button onClick={handleAlertClick}>Show alert</button>
+		</div>
+	)
+}
+
+// function Counter() {
+// 	const [count, setCount] = useState(0)
+
+// 	useEffect(() => {
+// 		setTimeout(() => {
+// 			console.log(`You clicked ${count} times`)
+// 		}, 3000)
+// 	})
+
+// 	return (
+// 		<div>
+// 			<p>You clicked {count} times</p>
+// 			<button onClick={() => setCount(count + 1)}>Click me</button>
+// 		</div>
+// 	)
+// }
+
+function Counter() {
+	const [state, dispatch] = useReducer(reducer, initialState)
+	const { count, step } = state
+
+	useEffect(() => {
+		const id = setInterval(() => {
+			dispatch({ type: 'tick' })
+		}, 1000)
+		return () => clearInterval(id)
+	}, [dispatch])
+
+	return (
+		<>
+			<h1>{count}</h1>
+			<input
+				value={step}
+				onChange={e => {
+					dispatch({
+						type: 'step',
+						step: Number(e.target.value)
+					})
+				}}
+			/>
+		</>
+	)
+}
+
+const initialState = {
+	count: 0,
+	step: 1
+}
+
+function reducer(state, action) {
+	const { count, step } = state
+	if (action.type === 'tick') {
+		return { count: count + step, step }
+	} else if (action.type === 'step') {
+		return { count, step: action.step }
+	} else {
+		throw new Error()
+	}
+}
+
+// export default User
+// export default Example
+export default Counter
