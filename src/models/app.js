dd98a0d7a8c51a68ed177f02753bedb309f2a065
yyeexin <248480904@qq.com>
2020-03-21 16:18:33 +0800

    commit

--- src/models/app.js
+++ src/models/app.js
@@ -13,11 +13,28 @@ export default {
 	subscriptions: {},
 	effects: {
 		*login({ payload }, { call, put, select }) {
-			const data = yield call(request, { url: userLogin, method: 'post', payload })
+			const res = yield fetch('/api/login', {
+				method: 'post',
+				body: JSON.stringify(payload)
+			})
+			const data = yield res.json()
+			console.log(data)
 			const { message, status } = data
 			if (message === 'success' || status === 200) {
 				yield put(routerRedux.push('/home'))
 			}
+			// .then(res => res.json())
+			// .then(res => {
+			// 	const { message, status } = res
+			// 	if (message === 'success' || status === 200) {
+			// 		routerRedux.push('/home')
+			// 	}
+			// })
+			// const data = yield call(request, { url: userLogin, method: 'post', payload })
+			// const { message, status } = data
+			// if (message === 'success' || status === 200) {
+			// 	yield put(routerRedux.push('/home'))
+			// }
 		},
 		*getMenus({ payload }, { call, put, select }) {
 			const data = yield call(request, { method: 'get', url: menus })
