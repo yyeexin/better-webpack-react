f21afde6aadb3f8e2b3c008fc9a0b97672970c8a
yyeexin <248480904@qq.com>
2020-03-24 10:54:02 +0800

    commit

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -5,8 +5,11 @@ import MenuLayout from './MenuLayout'
 const Layout = props => {
 	const { location, children } = props
 	const { pathname } = location
-	console.log(pathname, props, pathname === '/login')
-	return pathname === '/login' ? children : <MenuLayout {...props} />
+	console.log(pathname, pathname === '/login', props)
+	if (pathname === '/login') {
+		return <>{children}</>
+	}
+	return <MenuLayout {...props} />
 }
 
 export default withRouter(connect(({ app, dispatch }) => ({ app, dispatch }))(Layout))
