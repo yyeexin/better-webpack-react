644fb286da56da878ee4703cd670692f4b8d6219
yyeexin <248480904@qq.com>
2020-03-27 18:12:53 +0800

    commit

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,6 +1,6 @@
 import dayjsGenerateConfig from 'rc-picker/lib/generate/dayjs'
 import generateCalendar from 'antd/es/calendar/generateCalendar'
-import 'antd/es/calendar/style'
+import 'antd/es/calendar/style/css'
 
 const Calendar = generateCalendar(dayjsGenerateConfig)
 
